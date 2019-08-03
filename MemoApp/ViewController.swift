34ce38755e2189892f7bbbc0215dde2aa7442938
yuriko <yrk922@gmail.com>
2019-08-03 15:51:09 +0900

    削除機能の追加

--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -21,21 +21,32 @@ class ViewController: UIViewController, UITextViewDelegate {
         
         // AppDelegateを呼び出して変数に格納します。
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        //AppDelegateに定義したlastTextwosannsyousitexviewに格納
+        //AppDelegateに定義したlastTextを参照しtexviewに格納
         textView.text = appDelegate.lastText
         
 
     }
     
+    //消去ボタン
+    @IBAction func deleatButon(_ sender: Any) {
+        textView.text = ""
+           saveText()
+    }
+    
+    
+    
+    //textviewに入力された時の動作
     func textViewDidChange(_ textView: UITextView) {
+        saveText()
+    }
+    
+    //端末にデータを保存する
+    func saveText() {
         // AppDelegateを呼び出して変数に格納します。
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         
         // AppDelegateに記述した"lastText"に入力されている内容を格納します。
         appDelegate.lastText = textView.text
-    
-    }
-
-
 }
 
+}
